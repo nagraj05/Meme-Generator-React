c79cb429633c88519bb7100a16556cb8356286c1
Nagraj <tadingi.nagraj.nagraj7@gmail.com>
2023-05-27 08:47:02 +0530

    save

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -31,6 +31,36 @@ export default function Meme() {
       [name]: value,
     }));
   }
+  function saveMeme() {
+    const canvas = document.createElement("canvas");
+    const image = document.createElement("img");
+
+    image.src = memeImage.randomImage;
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const context = canvas.getContext("2d");
+      context.drawImage(image, 0, 0);
+      context.font = "30px Impact";
+      context.fillStyle = "white";
+      context.strokeStyle = "black";
+      context.lineWidth = 2;
+      context.textAlign = "center";
+
+      const topText = memeImage.topText.toUpperCase();
+      const bottomText = memeImage.bottomText.toUpperCase();
+
+      context.fillText(topText, canvas.width / 2, 40);
+      context.strokeText(topText, canvas.width / 2, 40);
+      context.fillText(bottomText, canvas.width / 2, canvas.height - 20);
+      context.strokeText(bottomText, canvas.width / 2, canvas.height - 20);
+
+      const link = document.createElement("a");
+      link.download = "meme.png";
+      link.href = canvas.toDataURL();
+      link.click();
+    };
+  }
 
   return (
     <main>
@@ -60,6 +90,9 @@ export default function Meme() {
         <h2 className="meme--text top">{memeImage.topText}</h2>
         <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
       </div>
+      <button className="form-button" onClick={saveMeme}>
+          Save Meme
+        </button>
     </main>
   );
 }
